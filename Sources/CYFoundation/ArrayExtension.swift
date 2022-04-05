93b7a48615e147d3a6f3a600808fc667028c536c
Luan <s384870sinyi@gmail.com>
2022-04-05 22:08:11 +0800

    Added functions to ArrayExtension.

--- Sources/CYFoundation/ArrayExtension.swift
+++ Sources/CYFoundation/ArrayExtension.swift
@@ -17,4 +17,38 @@ public extension Array {
 		return self[index]
 	}
 	
+	mutating func set(value: Element, at index: Int) {
+		guard !self.isEmpty,
+			self.count > index else {
+			return
+		}
+		self[index] = value
+	}
+	
+	func setting(value: Element, at index: Int) -> Array {
+		var result = self
+		guard !self.isEmpty,
+			self.count > index else {
+			return result
+		}
+		result[index] = value
+		return result
+	}
+
+}
+
+public extension Array where Element: Hashable {
+	
+	func uniqued() -> Array {
+		var buffer = Array()
+		var added = Set<Element>()
+		for elem in self {
+			if !added.contains(elem) {
+				buffer.append(elem)
+				added.insert(elem)
+			}
+		}
+		return buffer
+	}
+	
 }
